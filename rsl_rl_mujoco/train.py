89d8f4f3e8fcd957b691409951fc52f5fe3dcaac
jingbohan <jingbohan@bigai.ai>
2025-11-17 16:49:02 +0800

    fix visualization

--- rsl_rl_mujoco/train.py
+++ rsl_rl_mujoco/train.py
@@ -4,6 +4,7 @@ import gymnasium as gym
 from rsl_rl.runners import OnPolicyRunner
 
 from rsl_rl_mujoco.env_wrapper import GymMujocoWrapper
+from rsl_rl_mujoco.env_wrapper_Fix import GymMujocoWrapperFix
 
 from pathlib import Path
 
@@ -17,7 +18,15 @@ def train(cfg_path: str = str(Path(__file__).resolve().parent / "configs" / "def
     env_id = cfg["env"]["id"]
     num_envs = cfg["env"].get("num_envs", 4)
     envs = [gym.make(env_id) for _ in range(num_envs)]
-    env = GymMujocoWrapper(
+
+    # visualize the first environment
+    # envs = [
+    # gym.make(env_id, render_mode="human" if i == 0 else None)
+    # for i in range(num_envs)
+    # ]
+
+    # env = GymMujocoWrapper(
+    env = GymMujocoWrapperFix(
         envs,
         clip_actions=cfg["env"].get("clip_actions", 1.0),
         is_finite_horizon=cfg["env"].get("is_finite_horizon", True),
